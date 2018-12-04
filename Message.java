7239ec553025018ee782d9b8af4584be543ef00b
Nannapaneni Manoj Kumar <32504449+manoj0491@users.noreply.github.com>
2018-12-04 12:49:45 +0530

    Adding Message Cladd

---
+++ Message.java
@@ -0,0 +1,61 @@
+package com.tcs.rest.MessengerRest.model;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement
+public class Message {
+	private Long id;
+	private String message;
+	private Date created;
+	private String author;
+	private Map<Long,Comment> comments = new HashMap<>();
+	
+	public Message(Long id, String message, String author) {
+		this.id = id;
+		this.message = message;
+		this.created = new Date();
+		this.author = author;
+	}
+	public Message() {}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	@XmlTransient
+	public Map<Long, Comment> getComments() {
+		return comments;
+	}
+	public void setComments(Map<Long, Comment> comments) {
+		this.comments = comments;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date date) {
+		this.created = date;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+
+}
